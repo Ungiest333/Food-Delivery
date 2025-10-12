87f639b5ba84936476d5b3c0957e154d4688385f
Shresth <ds8957589@gmail.com>
2025-10-12 20:30:41 +0530

    frontend link

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,30 @@
-    // Create Server 
-    const express = require('express');
-    const cookieParser = require('cookie-parser')
-    const authRoutes = require("./routes/auth.routes")
-    const foodRoutes = require("./routes/food.routes")
-    const foodPartnerRoutes = require("./routes/food-partner.routes")
-    const cors = require("cors");
+// Create Server
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.routes");
+const foodRoutes = require("./routes/food.routes");
+const foodPartnerRoutes = require("./routes/food-partner.routes");
+const cors = require("cors");
 
-    const app = express();
-    app.use(cors({
-        origin: "http://localhost:5173",
-        credentials: true
-    }))
-    app.use(cookieParser());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
+const app = express();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // local development
+      "https://food-delivery-hebf.vercel.app", // deployed frontend on Vercel
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/food/", foodRoutes);
+app.use("/api/food-partner", foodPartnerRoutes);
 
-    app.get("/",(req,res)=>{
-    res.send("Hello world");
-    })
-    app.use('/api/auth', authRoutes);
-    app.use('/api/food/',foodRoutes);
-    app.use('/api/food-partner',foodPartnerRoutes);
-
-    module.exports = app;
+module.exports = app;
